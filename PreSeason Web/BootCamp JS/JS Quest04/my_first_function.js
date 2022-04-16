34b68c94e2c16026f7551d0057b322d1c8045e01
Mowahid Latif <mowahidlatif@hotmail.com>
2022-04-16 15:22:45 -0500

    Commiting QuestJS 04

---
+++ PreSeason Web/BootCamp JS/JS Quest04/my_first_function.js
@@ -0,0 +1,6 @@
+// function will print with console.log("my_first_function")
+function my_first_function() {
+  console.log("my_first_function");
+}
+
+my_first_function();
---
+++ PreSeason Web/BootCamp JS/JS Quest04/my_multiple_variables_multiple_type.js
@@ -0,0 +1,6 @@
+
+age = 34;
+name = "Luke";
+comma = ',';
+
+console.log("Hello " + name + comma + " I'm "+ age + " years old.");
